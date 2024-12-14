53609ebf8c6c28f4d35bba287ecda64bf5232170
Azizbek Shokirov <104092587+AzizbekShokirov@users.noreply.github.com>
2024-12-14 20:15:49 +0500

    Dev (#15)
    * Initial commit
    
    * Configured OTP for regisrter and password reset
    
    * Configured OTP (todo: change jwt token to sliding token)
    
    * Minor changes
    
    * Code refactoring and cleaning
    
    * SMTP configuration
    
    * Minor changes
    
    * "Minor changes"
    
    * requirements file update
    
    * Seperated views for 'get product' and 'filter product' logic
    
    * Udated admin panel
    
    * Solved issue with TokenRefresh View
    
    * Seperated like and dislike requests for favorites
    
    * minor change
    
    * Minor changes
    
    * Added is_favorite bool to product item in get list request
    
    * Minor changes
    
    * minor changes
    
    * minor change
    
    * Dev (#12) from main to dev (#13)
    
    * Initial commit
    
    * Configured OTP for regisrter and password reset
    
    * Configured OTP (todo: change jwt token to sliding token)
    
    * Minor changes
    
    * Code refactoring and cleaning
    
    * SMTP configuration
    
    * Minor changes
    
    * "Minor changes"
    
    * requirements file update
    
    * Seperated views for 'get product' and 'filter product' logic
    
    * Udated admin panel
    
    * Solved issue with TokenRefresh View
    
    * Seperated like and dislike requests for favorites
    
    * minor change
    
    * Minor changes
    
    * Added is_favorite bool to product item in get list request
    
    * Minor changes
    
    * minor changes
    
    * minor change
    
    * Update on jwt tokens
    
    * Updated cart serializers

--- api/cart/serializers.py
+++ api/cart/serializers.py
@@ -1,27 +1,56 @@
+from django.forms import ValidationError
+from products.serializers import ProductImageSerializer
 from rest_framework import serializers
 
 from cart.models import Cart, CartItem, Product
 
 
 class CartProductSerializer(serializers.ModelSerializer):
+    manufacturer = serializers.CharField(source="manufacturer.name")
+    images = ProductImageSerializer(many=True)
+
     class Meta:
         model = Product
-        fields = ["slug", "title", "price"]
+        fields = ["title", "price", "manufacturer", "images"]
+
+    def get_total_price(self, obj):
+        return obj.total_price()
 
 
-class CartItemSerializer(serializers.ModelSerializer):
-    product = CartProductSerializer(
-        read_only=True
-    )  # This will only show the product in responses, not in the request
+class CartItemGETSerializer(serializers.ModelSerializer):
+    product = CartProductSerializer()
     product_slug = serializers.SlugField(write_only=True)
+    quantity = serializers.IntegerField(min_value=1)
+    total_price = serializers.SerializerMethodField()
 
     class Meta:
         model = CartItem
-        fields = ["id", "product", "product_slug", "quantity"]
+        fields = ["product", "product_slug", "quantity", "total_price"]
+        read_only_fields = [
+            "product"
+        ]  # product is in the response, but not in the request
+
+    def get_total_price(self, obj):
+        return obj.total_price()
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+    product_slug = serializers.SlugField(write_only=True)
+    quantity = serializers.IntegerField(min_value=1)
+
+    class Meta:
+        model = CartItem
+        fields = ["product_slug", "quantity"]
+
+    def validate_slug(self, product_slug):
+        try:
+            return Product.objects.get(slug=product_slug)
+        except Product.DoesNotExist:
+            raise ValidationError(f"Product with slug '{product_slug}' does not exist.")
 
     def create(self, validated_data):
         product_slug = validated_data.pop("product_slug")
-        product = Product.objects.get(slug=product_slug)
+        product = self.validate_slug(product_slug)
         cart = self.context["cart"]
 
         cart_item, created = CartItem.objects.get_or_create(
@@ -39,7 +68,8 @@ class CartItemSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         if "product_slug" in validated_data:
             product_slug = validated_data.pop("product_slug")
-            instance.product = Product.objects.get(slug=product_slug)
+            product = self.validate_slug(product_slug)
+            instance.product = product
 
         instance.quantity = validated_data.get("quantity", instance.quantity)
         instance.save()
@@ -47,7 +77,7 @@ class CartItemSerializer(serializers.ModelSerializer):
 
 
 class CartSerializer(serializers.ModelSerializer):
-    cart_items = CartItemSerializer(many=True, read_only=True)
+    cart_items = CartItemGETSerializer(many=True, read_only=True)
     total_cost = serializers.SerializerMethodField()
 
     class Meta:
