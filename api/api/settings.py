4941e23d9fa472697fa8ce6da6ef5ad425deca4f
AzizbekShokirov <azizbekshokirov.dev@gmail.com>
2025-03-12 08:10:02 +0500

    Minor changes

--- api/api/settings.py
+++ api/api/settings.py
@@ -70,9 +70,12 @@ SIMPLE_JWT = {
 }
 
 REST_FRAMEWORK = {
-    "DEFAULT_AUTHENTICATION_CLASSES": (
+    "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    ),
+    ],
+    "DEFAULT_RENDERER_CLASSES": [  # Disable Browsable API
+        "rest_framework.renderers.JSONRenderer",
+    ],
     "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
